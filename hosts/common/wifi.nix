8307c01c144fa9349f4b1053803f0ed6260d4986
Binkersss <nathaniel.chappelle@proton.me>
2025-11-30 16:47:54 -0800

    ok starting over lol

--- hosts/common/wifi.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Enable NetworkManager for easy WiFi management
-  networking.networkmanager.enable = true;
-  
-  # Ensure user can manage network
-  users.users.binker.extraGroups = [ "networkmanager" ];
-}
