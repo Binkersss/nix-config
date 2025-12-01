fbaa8c71835e597b6df73ff7f36e85243f9a4005
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-11-30 23:28:01 -0800

    Some good stuff, moving home.nix into home/

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.binker = ./home.nix;
+            home-manager.users.binker = ./home/profiles/admin.nix;
 
 	    # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
