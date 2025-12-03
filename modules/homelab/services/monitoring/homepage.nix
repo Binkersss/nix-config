09232046392fdb7149b27716a56452f44635bcc1
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-02 22:31:59 -0800

    added homepage

--- modules/homelab/services/monitoring/homepage.nix
+++ modules/homelab/services/monitoring/homepage.nix
@@ -1,199 +1,138 @@
-{
-  config,
-  lib,
-  ...
-}:
+{ config, lib, pkgs, ... }:
+
+with lib;
+
 let
-  service = "homepage-dashboard";
   cfg = config.homelab.services.homepage;
-  homelab = config.homelab;
-in
-{
+  serverIP = "10.0.0.222";
+in {
   options.homelab.services.homepage = {
-    enable = lib.mkEnableOption {
-      description = "Enable ${service}";
-    };
-    misc = lib.mkOption {
-      default = [ ];
-      type = lib.types.listOf (
-        lib.types.attrsOf (
-          lib.types.submodule {
-            options = {
-              description = lib.mkOption {
-                type = lib.types.str;
-              };
-              href = lib.mkOption {
-                type = lib.types.str;
-              };
-              siteMonitor = lib.mkOption {
-                type = lib.types.str;
-              };
-              icon = lib.mkOption {
-                type = lib.types.str;
-              };
-            };
-          }
-        )
-      );
+    enable = mkEnableOption "Homepage dashboard";
+    
+    port = mkOption {
+      type = types.port;
+      default = 3000;
+      description = "Port for Homepage";
     };
   };
-  config = lib.mkIf cfg.enable {
-    services.glances.enable = true;
-    services.${service} = {
+
+  config = mkIf cfg.enable {
+    services.homepage-dashboard = {
       enable = true;
-      environmentFile = builtins.toFile "homepage.env" "HOMEPAGE_ALLOWED_HOSTS=${homelab.baseDomain}";
-      customCSS = ''
-        body, html {
-          font-family: SF Pro Display, Helvetica, Arial, sans-serif !important;
-        }
-        .font-medium {
-          font-weight: 700 !important;
-        }
-        .font-light {
-          font-weight: 500 !important;
-        }
-        .font-thin {
-          font-weight: 400 !important;
-        }
-        #information-widgets {
-          padding-left: 1.5rem;
-          padding-right: 1.5rem;
-        }
-        div#footer {
-          display: none;
-        }
-        .services-group.basis-full.flex-1.px-1.-my-1 {
-          padding-bottom: 3rem;
-        };
-      '';
+      
+      openFirewall = true;
+      
+      listenPort = cfg.port;
+      
       settings = {
+        title = "Homelab Dashboard";
+        
         layout = [
           {
-            Glances = {
-              header = false;
+            Media = {
               style = "row";
-              columns = 4;
-            };
-          }
-          {
-            Arr = {
-              header = true;
-              style = "column";
+              columns = 3;
             };
           }
           {
             Downloads = {
-              header = true;
-              style = "column";
-            };
-          }
-          {
-            Media = {
-              header = true;
-              style = "column";
-            };
-          }
-          {
-            Services = {
-              header = true;
-              style = "column";
+              style = "row";
+              columns = 2;
             };
           }
         ];
-        headerStyle = "clean";
-        statusStyle = "dot";
-        hideVersion = "true";
       };
-      services =
-        let
-          homepageCategories = [
-            "Arr"
-            "Media"
-            "Downloads"
-            "Services"
-            "Observability"
-            "Smart Home"
+      
+      services = [
+        {
+          Media = [
+            {
+              Jellyfin = {
+                icon = "jellyfin.png";
+                href = "http://${serverIP}:8096";
+                description = "Media Server";
+              };
+            }
+            {
+              Radarr = {
+                icon = "radarr.png";
+                href = "http://${serverIP}:7878";
+                description = "Movie Management";
+              };
+            }
+            {
+              Sonarr = {
+                icon = "sonarr.png";
+                href = "http://${serverIP}:8989";
+                description = "TV Show Management";
+              };
+            }
+            {
+              Prowlarr = {
+                icon = "prowlarr.png";
+                href = "http://${serverIP}:9696";
+                description = "Indexer Manager";
+              };
+            }
+            {
+              Bazarr = {
+                icon = "bazarr.png";
+                href = "http://${serverIP}:6767";
+                description = "Subtitle Management";
+              };
+            }
+            {
+              Jellyseerr = {
+                icon = "jellyseerr.png";
+                href = "http://${serverIP}:5055";
+                description = "Request Management";
+              };
+            }
           ];
-          hl = config.homelab.services;
-          homepageServices =
-            x:
-            (lib.attrsets.filterAttrs (
-              _name: value: value ? homepage && value.homepage.category == x
-            ) homelab.services);
-        in
-        lib.lists.forEach homepageCategories (cat: {
-          "${cat}" =
-            lib.lists.forEach (lib.attrsets.mapAttrsToList (name: _value: name) (homepageServices "${cat}"))
-              (x: {
-                "${hl.${x}.homepage.name}" = {
-                  icon = hl.${x}.homepage.icon;
-                  description = hl.${x}.homepage.description;
-                  href = "https://${hl.${x}.url}";
-                  siteMonitor = "https://${hl.${x}.url}";
-                };
-              });
-        })
-        ++ [ { Misc = cfg.misc; } ]
-        ++ [
-          {
-            Glances =
-              let
-                port = toString config.services.glances.port;
-              in
-              [
+        }
+        {
+          Downloads = [
+            {
+              Deluge = {
+                icon = "deluge.png";
+                href = "http://${serverIP}:8112";
+                description = "Torrent Client";
+              };
+            }
+          ];
+        }
+      ];
+      
+      widgets = [
+        {
+          resources = {
+            cpu = true;
+            memory = true;
+            disk = "/";
+          };
+        }
+        {
+          search = {
+            provider = "duckduckgo";
+            target = "_blank";
+          };
+        }
+      ];
+      
+      bookmarks = [
+        {
+          Developer = [
+            {
+              Github = [
                 {
-                  Info = {
-                    widget = {
-                      type = "glances";
-                      url = "http://localhost:${port}";
-                      metric = "info";
-                      chart = false;
-                      version = 4;
-                    };
-                  };
-                }
-                {
-                  "CPU Temp" = {
-                    widget = {
-                      type = "glances";
-                      url = "http://localhost:${port}";
-                      metric = "sensor:Package id 0";
-                      chart = false;
-                      version = 4;
-                    };
-                  };
-                }
-                {
-                  Processes = {
-                    widget = {
-                      type = "glances";
-                      url = "http://localhost:${port}";
-                      metric = "process";
-                      chart = false;
-                      version = 4;
-                    };
-                  };
-                }
-                {
-                  Network = {
-                    widget = {
-                      type = "glances";
-                      url = "http://localhost:${port}";
-                      metric = "network:enp2s0";
-                      chart = false;
-                      version = 4;
-                    };
-                  };
+                  abbr = "GH";
+                  href = "https://github.com/";
                 }
               ];
-          }
-        ];
-    };
-    services.caddy.virtualHosts."${homelab.baseDomain}" = {
-      useACMEHost = homelab.baseDomain;
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:${toString config.services.${service}.listenPort}
-      '';
+            }
+          ];
+        }
+      ];
     };
   };
 }
