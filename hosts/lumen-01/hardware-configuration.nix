0eec397fc07036a45a2aaca9f218126c3dd9e36a
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-02 20:12:16 -0800

    goddamn leading / typo

--- hosts/lumen-01/hardware-configuration.nix
+++ hosts/lumen-01/hardware-configuration.nix
@@ -28,7 +28,7 @@
     };
 
   fileSystems."/mnt/usbnas" = {
-    device = "dev/disk/by-uuid/aa917922-bd80-4690-bf12-18903f70c306";
+    device = "/dev/disk/by-uuid/aa917922-bd80-4690-bf12-18903f70c306";
     fsType = "ext4";
     options = [ "defaults" "rw" ]; 
   };
