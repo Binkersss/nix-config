afe5ab74d847fa3e6f4e172330e08a07067e2156
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-18 12:19:01 -0800

    Generation 92
    no ghostty protocol, trying kitty

--- home/profiles/binker.nix
+++ home/profiles/binker.nix
@@ -61,7 +61,7 @@
 
   home.file.".config/ranger/rc.conf".text = ''
     set preview_images true
-    set preview_images_method ghostty
+    set preview_images_method kitty
   '';
 
   home.file.".config/ranger/rifle.conf".text = ''
