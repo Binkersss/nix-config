9fc0c9e1be924afc277436634de1d2e0b55042cc
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-18 01:12:32 -0800

    Generation 52
    changing zsh config

--- home/profiles/binker.nix
+++ home/profiles/binker.nix
@@ -60,13 +60,38 @@
       f = "pay-respects";
     };
     history.size = 10000;
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "zsh-transient-prompt"
-      ];
-      theme = "robbyrussell";
-    };
+
+    initExtra = ''
+      # Compact Zsh Theme with Git Integration and Transient Prompt
+
+      autoload -Uz vcs_info
+      precmd_vcs_info() { vcs_info }
+      precmd_functions+=( precmd_vcs_info )
+      setopt prompt_subst
+
+      # Git status configuration
+      zstyle ':vcs_info:*' enable git
+      zstyle ':vcs_info:*' check-for-changes true
+      zstyle ':vcs_info:*' stagedstr '%F{green}●%f'
+      zstyle ':vcs_info:*' unstagedstr '%F{yellow}●%f'
+      zstyle ':vcs_info:git:*' formats ' %F{cyan}%f %F{magenta}%b%f %u%c'
+      zstyle ':vcs_info:git:*' actionformats ' %F{cyan}%f %F{magenta}%b%f %F{red}%a%f %u%c'
+
+      # Main prompt with current directory and git info
+      PROMPT='%F{blue}%~%f$vcs_info_msg_0_ %F{green}❯%f '
+
+      # Transient prompt - replaces previous prompts with minimal version
+      _prompt_transient() {
+        PROMPT='%F{green}❯%f '
+        zle reset-prompt
+        PROMPT='%F{blue}%~%f$vcs_info_msg_0_ %F{green}❯%f '
+      }
+
+      zle -N _prompt_transient
+      bindkey '^M' _prompt_transient
+
+      # Right prompt with execution time and exit status
+      RPROMPT='%F{242}%*%f'
+    '';
   };
 }
