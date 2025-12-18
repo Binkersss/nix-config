9d4bf1739b75d7dbef895c349ca82697c3af0c19
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-17 22:47:56 -0800

    i still managed to typo lol

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -975,7 +975,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter.configs').setup {
+      require('nvim-treesitter.config').setup {
         ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
         auto_install = true,
         highlight = {
