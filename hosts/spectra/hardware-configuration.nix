9a25040477f7419c886c31196280057e52b4add9
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-18 10:09:27 -0800

    Generation 83
    adding nvidia, dri always on with unstable

--- hosts/spectra/hardware-configuration.nix
+++ hosts/spectra/hardware-configuration.nix
@@ -37,12 +37,14 @@
 
   hardware.opengl = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    # driSupport = true;
+    # driSupport32Bit = true;
   };
 
   hardware.nvidia = {
     modesetting.enable = true;
+
+    open = true;
 
     # prime = {
     #   offload = {
