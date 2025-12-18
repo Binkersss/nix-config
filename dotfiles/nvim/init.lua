42237b7bb295b08c75e73257917fd0f3348fe4e5
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-18 00:33:58 -0800

    Generation 48
    change ft

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -799,7 +799,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         nix = { 'alejandra' },
-        md = { 'prettier' },
+        markdown = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
