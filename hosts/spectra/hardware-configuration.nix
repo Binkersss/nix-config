6a72fa0fdf136a043c1007906b9fb5030aa27a72
Nathaniel Chappelle <nathaniel@chappelle.dev>
2025-12-18 10:08:10 -0800

    Generation 83
    changing

--- hosts/spectra/hardware-configuration.nix
+++ hosts/spectra/hardware-configuration.nix
@@ -33,7 +33,9 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  hardware.graphics = {
+  hardware.graphics.enable = true;
+
+  hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
