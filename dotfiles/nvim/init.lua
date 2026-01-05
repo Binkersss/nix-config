7f71770033a8f97d39405692e2cd4b9d2af8e21e
Nathaniel Chappelle <nathaniel@chappelle.dev>
2026-01-05 11:16:31 -0800

    Generation 142
    adding marksman

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -768,6 +768,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        marksman = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
